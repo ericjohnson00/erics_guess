067cd271a36fc5ef6856fe8d28a59c13047d9cc2
Eric Johnson <154352503+ericjohnson00@users.noreply.github.com>
2024-01-06 16:01:46 -0600

    Add files via upload

--- erics_guess.py
+++ erics_guess.py
@@ -1,9 +1,15 @@
 import random
+import time
 
-print("Created by Eric Johnson 1/4/2024")
+
+print("•°¿?¿ERIC'S GUESS¿?¿°•")
+time.sleep(3)
 print()
+print("Created by Eric Johnson 1/4/2024")
+time.sleep(2)
 print()
 print("•○● ericeugene.com ●○•")
+time.sleep(2)
 print()
 
 def erics_guess():
@@ -14,11 +20,16 @@ def erics_guess():
         guess = int(input("Guess a number between 1 and 100 : "))
         
         if guess == number:
+            print()
             print("You got it!!!")
-            play_again = input("Would you like to play again? y or n :")
+            print()
+            play_again = input("Would you like to play again? y or n : ")
             if play_again.lower() == "y":
                 erics_guess()
             else:
+                time.sleep(2)
+                print("That was fun!")
+                time.sleep(2)
                 print("Goodbye")
                 break
                 
@@ -27,7 +38,7 @@ def erics_guess():
         else: 
                 print("To high, try again")
         
-play = input("Would you like to play a game? y or n :")
+play = input("Would you like to play a game? y or n : ")
 
 if play.lower() == "y":
     erics_guess()
